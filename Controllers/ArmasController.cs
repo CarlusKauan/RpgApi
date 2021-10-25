ce009ea4cbfdeeeb26c8dd01515b34b08712a501
carloskauan <carloskauan190@gmail.com>
2021-10-25 19:31:20 -0300

    Desafio aula 11 conpleto

--- Controllers/ArmasController.cs
+++ Controllers/ArmasController.cs
@@ -56,15 +56,23 @@ namespace RpgApi.Controllers
                     throw new System.Exception("Dano da arma não pode ser ser 0!");
                 }
 
-                Personagem p = await _context.Personagens
+                Personagem personagem = await _context.Personagens
                               .FirstOrDefaultAsync(p => p.Id == novaArma.PersonagemId);
-                if(p == null)
+
+                if(personagem == null)
                 {
-                    throw new System.Exception("Não existe personagem com ID informado");
+                    throw new System.Exception("Seu usuário não contem personagens com Id do Personagem informado");
                 }
+
+                Arma buscaArma = await _context.Armas
+                                .FirstOrDefaultAsync(a => a.PersonagemId == novaArma.PersonagemId);
+
+                if(buscaArma != null)
+                    throw new System.Exception("O personagem selecionado já contém uma arma atribuída a ele.");
 
                 await _context.Armas.AddAsync(novaArma);
                 await _context.SaveChangesAsync();
+
                 return Ok(novaArma.Id);
             }
             catch (System.Exception ex)
